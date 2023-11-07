86edc256f014b964cb1eab1ccbe2189016294a4c
Eason Ong <eong@lcs.on.ca>
2023-11-07 14:42:59 -0500

    Initial Commit

---
+++ Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by Ong Eason on 7/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
